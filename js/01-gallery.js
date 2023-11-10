49c42e8852c1c3f9673f1c9d69652f3dea8d7a71
polinamln <polina77979.la@gmail.com>
2023-11-10 20:22:13 +0100

    3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,6 +9,7 @@ const list = document.querySelector(".gallery");
 list.addEventListener('click', handleClick);
 
 function handleClick(event) {
+    event.preventDefault();
     if (event.target.nodeName !== "IMG") {
         return;
     }
@@ -31,11 +32,11 @@ createMarcup(galleryItems);
 function createMarcup(items) {
     const htmlArr = items.map(item =>
         `<li class="gallery__item">
-            <a class="gallery__link" href="large-image.jpg">
+            <a class="gallery__link" href="${item.original}">
                 <img
                     class="gallery__image"
                     src="${item.preview}"
-                    data-source="${item.original}
+                    data-source="${item.original}";
                     alt="${item.description}"
                 />
             </a>
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+
+
+
+list.addEventListener('click', handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const gallery = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+        captionPosition:'bottom'
+    });
+
+    const imgCaption = event.target.dataset.caption;
+    const currentImg = event.target.dataset.source;
+
+    gallery.items = [
+        {
+            src: currentImg,
+            type: 'image',
+            caption: imgCaption,
+        },
+    ];
+}
+
+
+createMarcup(galleryItems);
+
+function createMarcup(items) {
+    const htmlArr = items.map(item =>
+`<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}" data-caption="${item.description}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+    );
+
+    list.insertAdjacentHTML('beforeend', htmlArr.join(''));
+};
+
+
+
+
+console.log(galleryItems);
+
+
+
 console.log(galleryItems);
